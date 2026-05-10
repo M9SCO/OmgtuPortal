dc066fe147f9c6aaeeab37c29c2ca39c70399931
M9SCO <chr.grigorii@gmail.com>
2026-05-10 13:12:44 +0600

    feature: response static from /

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+var distPath = Path.Combine(app.Environment.ContentRootPath, ".frontend", "dist");
+
+if (Directory.Exists(distPath))
+{
+    var fileProvider = new PhysicalFileProvider(distPath);
+
+    app.UseDefaultFiles(new DefaultFilesOptions { FileProvider = fileProvider });
+    app.UseStaticFiles(new StaticFileOptions { FileProvider = fileProvider });
+}
+
 app.MapGet("/api/health", () => Results.Ok());
+
+app.Map("/api/{**slug}", () => Results.NotFound());
+
+if (Directory.Exists(distPath))
+{
+    app.MapFallbackToFile("index.html", new StaticFileOptions
+    {
+        FileProvider = new PhysicalFileProvider(distPath)
+    });
+}
 
 app.Run();
